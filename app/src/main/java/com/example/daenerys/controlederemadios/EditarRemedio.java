eed29f961f1ad1745ea4c33ebff50be4f0b2b826
daenerys <pereirajefferson06@gmail.com>
2017-11-03 11:34:35 -0200

    Exemplo da editar remedios e listá-los.

---
+++ app/src/main/java/com/example/daenerys/controlederemadios/EditarRemedio.java
@@ -0,0 +1,13 @@
+package com.example.daenerys.controlederemadios;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditarRemedio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar_remedio);
+    }
+}
